eddc8f0ed69e54d773adf3bdb66a195508278160
Daniel <dnaforsolutions@gmail.com>
2023-10-08 22:05:44 +0200

    save changes

--- src/attribute/relations/option/entities/option.entity.ts
+++ src/attribute/relations/option/entities/option.entity.ts
@@ -1,13 +1,6 @@
 import { Attributes } from '@src/attribute/entities/attributes.entity';
 import { MecBaseEntity } from '@src/base/entity/base.entity';
-import {
-    Column,
-    Entity,
-    Index,
-    JoinColumn,
-    ManyToOne,
-    OneToMany,
-} from 'typeorm';
+import { Column, Entity, Index, ManyToOne } from 'typeorm';
 
 export const AttributeOptionIndexPrefix = 'ik_attribute_option_index';
 export const AttributeOptionIndexKeys: string[] = [
@@ -30,18 +23,6 @@ export class Option extends MecBaseEntity {
         nullable: false,
     })
     relatedAttribute: number;
-
-    // @OneToMany(() => StoreOption, (storeOption) => storeOption, {
-    //     cascade: false,
-    //     eager: false,
-    //     nullable: true,
-    // })
-    // @JoinColumn({
-    //     foreignKeyConstraintName: 'fk_attribute_store_view_options',
-    // })
-    // storeOptions: StoreOption[];
-    // @RelationId((option: Option) => option.storeViewOptions)
-    // storeViewOptionsIds: number[];
 }
 
 // For multi store I need to find way how to create new table every time new store is created!
